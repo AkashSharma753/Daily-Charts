1c10676c1b4122c02bdd9951301b8e06e7e14ba5
Akash Sharma <90839531+AkashSharma753@users.noreply.github.com>
2022-03-03 17:59:12 +0530

    Add files via upload

---
+++ model/user.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username : {
+    type: String,
+    required : true,
+},
+email : {
+    type: String,
+    required : true,
+},
+password: 
+{
+    type: String,
+
+},
+isVerified:
+{
+   type: Boolean,
+   default : false, 
+},
+
+googleID:{
+    type: String,
+},
+
+provider :{
+    type: String,
+    required : true,
+},
+
+});
+
+module.exports = mongoose.model('user', userSchema);
